b27b4119655cbbb9a3997b4549af7f4485067834
Eldos Yksanov <eldosyksanov@gmail.com>
2024-12-28 18:58:56 +0600

    testing project

--- ControlWork9/obj/Debug/net8.0/ControlWork9.AssemblyInfo.cs
+++ ControlWork9/obj/Debug/net8.0/ControlWork9.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControlWork9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8799713cfd59c8033141767818a5f9ab80bf62f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d1f2f059079ee5a2caa904e021e98ad6c1093cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControlWork9")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControlWork9")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
