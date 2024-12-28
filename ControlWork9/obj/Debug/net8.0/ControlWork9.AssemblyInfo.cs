0ea7f139482ac4b18e1251192b4f118d81c87c1e
Eldos Yksanov <eldosyksanov@gmail.com>
2024-12-28 20:45:28 +0600

    also changed code View

--- ControlWork9/obj/Debug/net8.0/ControlWork9.AssemblyInfo.cs
+++ ControlWork9/obj/Debug/net8.0/ControlWork9.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControlWork9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fd5e827e1eb9de4dd3af85f90ddf801c7596077")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e77e25f3b00f6f20bebdf4404a030cdc79d83d0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControlWork9")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControlWork9")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
